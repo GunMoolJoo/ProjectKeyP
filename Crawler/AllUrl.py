a7f3c76a803991a5c6b16651cc45e4bb037c3c1b
loginshin <95666408+loginshin@users.noreply.github.com>
2022-08-10 09:55:13 +0900

    meca

--- Crawler/AllUrl.py
+++ Crawler/AllUrl.py
@@ -1,43 +1,54 @@
-### 초기 세팅 ###
-# pip install --upgrade firebase-admin
-# 터미널에 위 코드를 입력
+from itertools import product
+import requests as rq
+from bs4 import BeautifulSoup
+from datetime import datetime
 
-### cloud firestore 를 사용하기 위한 초기 코드 ###
-from tokenize import String
+
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-# Use a service account
-cred = credentials.Certificate('D:\gunmoojoo-29a642ad200c.json')
-firebase_admin.initialize_app(cred)
 
+# Firebase 다운 받은 키 위치 저장
+cred = credentials.Certificate(
+    "C:\project\ProjectKeyP\gunmoojoo-29a642ad200c.json")
+firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-doc_ref = db.collection(u'users').document(u'aaa')
+dic_ref = db.collection(u'users').document(u'aaa')
 
-for i in doc_ref.get():
-  print(i)
-
-
-print(a)
+# 정보를 담을 빈 리스트
+product_info = []
 
 
+now = datetime.now()  # 현재시간을 알려준다.
+print(now)
 
-# # Create an Event for notifying main thread.
-# import threading
+
+def craw():
+    url = "https://news.naver.com/"
+
+    # url에서 파싱하기
+    res = rq.get(url)
+    soup = BeautifulSoup(res.text, 'lxml')
+
+    # title값 가지고오기
+    title = soup.head.title.text.strip()
+    print(title)
+
+    # 전체에서 가지고오기
+    info = soup.select('p')
+
+    keyword = "홍수"
+
+    for i in info:
+        if keyword in i.text.strip():
+            product_info.append(i.text.strip())
+            product_info.append("============================")
+
+    return product_info
 
 
-# callback_done = threading.Event()
-
-# # Create a callback on_snapshot function to capture changes
-# def on_snapshot(doc_snapshot, changes, read_time):
-#     for doc in doc_snapshot:
-#         print(f'Received document snapshot: {doc.id}')
-#     callback_done.set()
-
-# doc_ref = db.collection(u'cities').document(u'SF')
-
-# # Watch the document
-# doc_watch = doc_ref.on_snapshot(on_snapshot)
+list = craw()
+print(list)
