9c19aac42c294ea6f51bdd591f91370f8db529e2
jeonjuho23 <91320503+jeonjuho23@users.noreply.github.com>
2022-08-17 07:56:24 +0900

    real final crawler
    ewohgwoiwo

--- Crawler/connect.py
+++ Crawler/connect.py
@@ -4,11 +4,16 @@
 
 from concurrent.futures import ThreadPoolExecutor
 import concurrent.futures
+from email import header
+from email.headerregistry import HeaderRegistry
+from glob import glob
 from multiprocessing import Pool
 import multiprocessing as mp
 from threading import Thread
 import threading
 import time
+from urllib import request, response
+from wsgiref import headers
 import psutil
 ## psutil pip install psutil 로 깔아줘야함
 
@@ -109,29 +114,153 @@ def find_keyword_in_crawling_data(url, crawling_data, keyword):
     ## 크롤링된 데이터들 중에 keyword를 포함한 문장이 있으면 데이터베이스에 update( 중복이 있으면 저장하지 않음 )
     for line in crawling_data:
         if keyword in line.text.strip():
-            doc_ref = doc_ref = db.collection('crawlingUrl').document(url).collection(keyword).document('data')
+            doc_ref = doc_ref = db.collection('crawlingUrl').document(url).collection(keyword)
             ## DB에 데이터 저장하기
             ## 저장할 데이터 확실하게 결정 후 수정 필요!
             
             ## 이거는 입력받은 url에서 찾은 데이터
-            doc_ref.update({u'data' : firestore.ArrayUnion([{line.text.strip():line.get('href')}])})
+            doc_ref.document('data').update({'data' : firestore.ArrayUnion([{line.text.strip():line.get('href')}])})
+            
+            ## 본문내용 요약하기
+            
+            summary_text = summary(line.get('href'))
             
             ## 본문을 요약한 후 DB에 넣을 데이터
-            # use_TLDR_This()
+            doc_ref.document('summary').update({'summary' : firestore.ArrayUnion([{line.get('href'):summary_text}])})
             
             
             
 ## craw main text - 본문 크롤링하기  #################################
-def craw_main_text():
-    print()
+
+## use  Papago API  #################################################
+#### papago API url
+papago_url = "https://openapi.naver.com/v1/papago/n2mt"
+#### papago API key
+naver_client_id = 'IA_L6oYvsrjg2kqQFruz'
+naver_client_secret = 'S2gY5DO1ea'
+
+#### 한글 -> 영어 번역하기 ###############################
+def use_papago_for_kor_to_eng(trans_text):
     
+    ## API에 전송할 데이터
+    payload = {
+        'source' : 'ko',
+        'target' : 'en',
+        'text' : trans_text,
+    }
     
+    headers = {
+        "content-type": "application/json",
+        "X-Naver-Client-Id": naver_client_id,
+        "X-Naver-Client-Secret": naver_client_secret,
+    }
+    
+    global papago_url
+    
+    ## API에 데이터 전송
+    response = rq.request("POST", papago_url, json=payload, headers=headers)
+    
+    ## 영어로 번역된 문장 반환
+    eng_text = response.json()['message']['result']['translatedText']
+    
+    return eng_text
+
+#### 영어 -> 한글 번역하기 ###############################
+def use_papago_for_eng_to_kor(trans_text):
+## API에 전송할 데이터
+    payload = {
+        'source' : 'en',
+        'target' : 'ko',
+        'text' : trans_text,
+    }
+    
+    headers = {
+        "content-type": "application/json",
+        "X-Naver-Client-Id": naver_client_id,
+        "X-Naver-Client-Secret": naver_client_secret,
+    }
+    
+    global papago_url
+    
+    ## API에 데이터 전송
+    response = rq.request("POST", papago_url, json=payload, headers=headers)
+    
+    ## 한글로 번역된 문장 반환
+    kor_text = response.json()['message']['result']['translatedText']
+    
+    return kor_text
+
 ## use  TLDR This (Open API)  #######################################
-def use_TLDR_This():
-    print()
+#### 본문 크롤링 ########################################
+def use_TLDR_This_for_crawling_main_text(crawling_url):
     
+    ## 사용할 기능의 url
+    url = "https://tldrthis.p.rapidapi.com/v1/model/abstractive/summarize-url/"
     
+    ## API로 보낼 정보
+    payload = {
+        "url" : crawling_url,
+        "min_length": 100, # 최소 길이
+        "max_length": 250, # 최대 길이
+        "is_detailed":True # 한 문장으로 반환할 것인지 여부
+    }
     
+    headers = {
+        'content-type': 'application/json',
+        'X-RapidAPI-Key': '4a850f4a03mshc080ed0be83dc09p1e245cjsn804d4267423a',
+        'X-RapidAPI-Host': 'tldrthis.p.rapidapi.com'
+    }
+    
+    ## API로 url 정보 보내기
+    response = rq.request("POST", url, json=payload, headers=headers)
+    
+    ## 본문 크롤링 반환받기
+    json_data = response.json()
+    content = json_data['article_text']
+    return content
+
+#### 본문내용 요약하기 ####################################
+def use_TLDR_This_for_summary_main_text(content):
+    
+    ## 사용할 기능의 url
+    url = 'https://tldrthis.p.rapidapi.com/v1/model/abstractive/summarize-text/'
+    
+    ## API로 보낼 정보
+    payload = {
+        'text' : content,
+        'min_length' : 30,
+        'max_length' : 100
+    }
+    
+    headers = {
+        'content-type': 'application/json',
+        'X-RapidAPI-Key': '4a850f4a03mshc080ed0be83dc09p1e245cjsn804d4267423a',
+        'X-RapidAPI-Host': 'tldrthis.p.rapidapi.com'
+    }
+    
+    ## API로 본문 내용 보내기
+    response = rq.request("POST", url, json=payload, headers=headers)
+    
+    ## 요약된 정보 반환받기
+    json_data = response.json()
+    summary = json_data['summary']
+    return summary
+
+
+################ summary ###################
+def summary(crawling_url):
+    ## crawling main text
+    kor_main_text = use_TLDR_This_for_crawling_main_text(crawling_url)
+    ## translate main text ( ko -> en )
+    eng_main_text = use_papago_for_kor_to_eng(kor_main_text)
+    ## summary main text
+    eng_summary_text = use_TLDR_This_for_summary_main_text(eng_main_text)
+    ## translate summary text ( en -> ko )
+    kor_summary_text = use_papago_for_eng_to_kor(eng_summary_text)
+    
+    return kor_summary_text
+
+
 ### Multi Processing #################################################################    
 
 ############### porcess #########################################################
